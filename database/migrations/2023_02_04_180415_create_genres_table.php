389a79ad695fc2c626f6ee6d81d7cdba07f7414e
Yenishdev <shamyradovyenis16@gmail.com>
2023-02-05 10:29:47 +0500

    1.0.1

--- database/migrations/2023_02_04_180415_create_genres_table.php
+++ database/migrations/2023_02_04_180415_create_genres_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('genres', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
         });
     }
 
